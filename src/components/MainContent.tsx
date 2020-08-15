b00b53514eedd340f89faf28f0ffb7c938e3acdb
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-08-16 02:40:35 +0900

    add MainContent component

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import DataTable from './DataTable';
+
+const MainContent: React.FC = () => {
+  return (
+    <>
+      <DataTable />
+    </>
+  );
+};
+
+export default MainContent;
